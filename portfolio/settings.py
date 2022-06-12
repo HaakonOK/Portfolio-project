b47bdf3f94b6a2973513e01a0bf16df4b84df395
HaakonOKristiansen <haakon@nordtromsbolig.no>
2022-06-12 15:15:11 +0200

    Added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,3 +134,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
